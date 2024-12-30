fa2e95218b57ce9f808f4cf35b9c822f3c039ba2
Venkat <venkatgithub676@gmail.com>
2024-12-30 16:35:52 +0000

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Route, Switch} from 'react-router-dom'
 
 import './App.css'
+import Home from './components/Home'
 import Login from './components/Login'
 // Replace your code here
 const App = () => (
   <>
     <Switch>
+      <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
     </Switch>
   </>
