80a734bc93ba65a447ab9f7dfc58c84ae4b16991
Venkat <venkatgithub676@gmail.com>
2025-01-06 10:34:53 +0000

    01-06-2025 4:04pm

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,49 @@
 import {Component} from 'react'
+import {IoMdClose, IoMdSearch} from 'react-icons/io'
+
 import Header from '../Header'
+
+import {
+  HomeCon,
+  HomePopupCon,
+  TempPopupSection,
+  TempPopupSectionHeading,
+  TempPopupSectionImg,
+  TempPopupSectionBtn,
+  CloseButton,
+  SearchCon,
+  InputSearch,
+  SearchLabel,
+} from './styledComponents'
 
 class Home extends Component {
   render() {
-    return <Header />
+    return (
+      <>
+        <Header />
+        <HomeCon>
+          <HomePopupCon>
+            <TempPopupSection>
+              <TempPopupSectionImg src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png" />
+              <TempPopupSectionHeading>
+                Buy Nxt Watch Premium prepaid plans with UPI
+              </TempPopupSectionHeading>
+              <TempPopupSectionBtn>GET IT NOW</TempPopupSectionBtn>
+            </TempPopupSection>
+            <CloseButton>
+              <IoMdClose />
+            </CloseButton>
+          </HomePopupCon>
+
+          <SearchCon>
+            <InputSearch placeholder="Search" />
+            <SearchLabel>
+              <IoMdSearch />
+            </SearchLabel>
+          </SearchCon>
+        </HomeCon>
+      </>
+    )
   }
 }
 
--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -1,1 +1,65 @@
 import styled from 'styled-components'
+
+export const HomeCon = styled.div``
+
+export const HomePopupCon = styled.div`
+  width: 95%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 10px 50px 10px 50px;
+`
+export const TempPopupSection = styled.div`
+  width: 70%;
+`
+
+export const TempPopupSectionImg = styled.img`
+  width: 100%;
+  max-width: 200px;
+`
+export const TempPopupSectionHeading = styled.p`
+  font-family: Roboto;
+  font-size: 25px;
+  line-height: 2;
+`
+export const TempPopupSectionBtn = styled.button`
+  width: 100px;
+  height: 40px;
+  border-style: solid;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: bold;
+  margin-top: 20px;
+`
+
+export const CloseButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  font-size: 25px;
+`
+
+export const SearchCon = styled.div`
+  width: 100%;
+  padding: 20px 30px 20px 30px;
+  display: flex;
+  align-items: center;
+`
+
+export const InputSearch = styled.input`
+  width: 80%;
+  height: 40px;
+  font-family: Roboto;
+  font-size: 20px;
+  padding-left: 20px;
+`
+
+export const SearchLabel = styled.label`
+  height: 40px;
+  font-size: 25px;
+  width: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #d7dfe9;
+`
